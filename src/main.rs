e3dda4846ec2f6ae63e70d2b5b8a116b8b6559ba
rifai-rizqi3 <rnrifai12@gmail.com>
2024-05-17 11:15:25 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, ki!");
+    let number = 8;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
